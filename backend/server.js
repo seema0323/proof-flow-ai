47b9a1f9da26486a4049a57876ac66a8e67523f5
seema0323 <yadavseema0323@gmail.com>
2026-09-05 00:56:23 +0530

    feat: connect MongoDB and add user signup API

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,42 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./config/db");
+const User = require("./models/User");
+
 const app = express();
+
+app.use(express.json());
+
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("ProofFlow AI Backend is running");
 });
+
+app.post("/user", (req, res) => {
+  res.json({
+    message: "User data received",
+    data: req.body,
+  });
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+
+    res.status(201).json({
+      message: "User created successfully",
+      user: user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Signup failed",
+      error: error.message,
+    });
+  }
+});
+
 app.listen(5000, () => {
   console.log("ProofFlow backend running on port 5000");
 });
