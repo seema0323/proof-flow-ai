fc06ae17fceb1c2725855d9b4c6904acc336bc77
seema0323 <yadavseema0323@gmail.com>
2026-09-05 15:45:12 +0530

    feat: add secure authentication with JWT

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
-
+const authMiddleware = require("./middleware/authMiddleware");
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const connectDB = require("./config/db");
 const User = require("./models/User");
 
@@ -10,24 +13,37 @@ app.use(express.json());
 
 connectDB();
 
+// Test API
 app.get("/", (req, res) => {
   res.send("ProofFlow AI Backend is running");
 });
 
-app.post("/user", (req, res) => {
-  res.json({
-    message: "User data received",
-    data: req.body,
-  });
-});
-
+// Signup API
 app.post("/signup", async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
     res.status(201).json({
       message: "User created successfully",
-      user: user,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (error) {
     res.status(500).json({
@@ -35,6 +51,56 @@ app.post("/signup", async (req, res) => {
       error: error.message,
     });
   }
+});
+
+// Login API
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Login failed",
+      error: error.message,
+    });
+  }
+});
+app.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed successfully",
+    user: req.user,
+  });
 });
 
 app.listen(5000, () => {
